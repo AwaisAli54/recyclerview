9188ad03fd2d92bdded1c7a4ed7cd50eb2236490
AwaisAli54 <121876929+AwaisAli54@users.noreply.github.com>
2023-06-21 20:05:56 +0500

    assignment

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,54 +1,86 @@
 package com.example.recycleview;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Friends> friendsList = new ArrayList<>();
-    RecyclerView recyclerView;
-    RecyclerView.Adapter adapter;
-    RecyclerView.LayoutManager layoutManager;
-
+    TextView textView;
+    Button button1, button2 , button3 , button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Friends f0 = new Friends(1,"Asad", 1980, "Giglgit",R.drawable.d);
-        Friends f1 = new Friends(2,"Zubair", 1981, "Lahore",R.drawable.c);
-        Friends f2 = new Friends(3,"Musa", 1980, "Quetta",R.drawable.b);
-        Friends f3 = new Friends(4,"Nadeem",1987,"Peshawar",R.drawable.a);
-        Friends f4 = new Friends(5,"Shahid", 1980, "Karachi",R.drawable.c);
-        Friends f5 = new Friends(6,"Zia",1987,"Faisalabad ",R.drawable.a);
-        Friends f6 = new Friends(7,"Badar", 1980, "Rawalpindi",R.drawable.d);
-        Friends f7 = new Friends(8,"Hashim",1997,"Karachi",R.drawable.b);
-        Friends f8 = new Friends(9,"Salman",1980,"Quetta",R.drawable.c);
-        Friends f9 = new Friends(10,"Rizwan",1982,"Kasur",R.drawable.d);
-        Friends f10 = new Friends(11,"Junaid",1977,"Islamabad",R.drawable.a);
-        Friends f11 = new Friends(12,"Waseem",1967,"Rawalpindi",R.drawable.b);
 
-        friendsList.addAll(Arrays.asList(new Friends[]{f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11}));
-        recyclerView = findViewById(R.id.recylerViewStudent);
+        textView = findViewById(R.id.textView3);
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+
+        Calendar calendar;
+        SimpleDateFormat dateFormat;
+        String date;
 
 
-        recyclerView.setHasFixedSize(true);
+        calendar = Calendar.getInstance();
 
-        //LinearLayoutManager GridLayoutManager
-        layoutManager = new LinearLayoutManager(MainActivity.this);
-//        layoutManager = new LinearLayoutManager(MainActivity.this,
-//                LinearLayoutManager.HORIZONTAL,
-//                false);
-        recyclerView.setLayoutManager(layoutManager);
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        date = dateFormat.format(calendar.getTime());
+        textView.setText(date);
 
-        adapter = new myRecyclerViewAdapter(friendsList) ;
-        recyclerView.setAdapter(adapter);
-        //adapter.notifyDataSetChanged();
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this , MainActivity6.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , MainActivity5.class);
+                startActivity(intent);
+            }
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , MainActivity3.class);
+                startActivity(intent);
+
+            }
+        });
+
+
 
 
     }
